5766228121be16563226d8ceddde645e0fd61176
Walker Aumann <waumann@gmail.com>
2022-12-26 18:20:37 -0800

    Done with Python

--- day20b.py
+++ day20b.py
@@ -2,40 +2,42 @@
 
 import sys
 
-out = [int(x) * 811589153 for x in open('input.day20.test')]
-order = out.copy()
+MULT = 811589153
+ROUNDS = 10
+# MULT, ROUNDS = 1, 1
 
-length = len(out)
+out = [int(x) * MULT for x in open('input.day20.txt')]
+order = list(range(len(out)))
 
-def move(input, value):
-  if value == 0: return input
-  work = input.copy()
-  loc = input.index(value)
-  keep = work.pop(loc)
-  work.insert((loc + keep) % length, keep)
-  return work
+length = len(out) - 1
 
-print(out)
+def move(input, idx):
+  loc = input.index(idx)
+  ptr = input.pop(loc)
+  raw_position = loc + out[idx]
+  net_position = raw_position % length
+  #print('Moving value by %d (was %d)' % (net_position, raw_position))
+  #print('======================')
+  input.insert(net_position, ptr)
+  return input
 
-for _ in range(10):
-  for i in order:
-    out = move(out, i)
+for r in range(ROUNDS):
+  print('Round %d' % r)
+  for i in range(len(order)):
+    #if r == 1: print('%3d: Moving %d' % (i, order[i]))
+    order = move(order, i)
+    #if r < 1: continue
+    #if i < 6: continue
     #pos = out.index(0)
     #out = out[pos:] + out[:pos]
     #for x in out: print(x)
-    #print('============')
-  #sys.exit(0)
+    #sys.exit(0)
 
 # print('Final = %s' % out)
-start = out.index(0)
-i1 = (start + 1000) % len(out)
-print('i1 = %d + 1000 mod %d = %d' % (start, len(out), i1))
-i2 = (start + 2000) % len(out)
-print('i2 = %d + 2000 mod %d = %d' % (start, len(out), i2))
-i3 = (start + 3000) % len(out)
-print('i3 = %d + 3000 mod %d = %d' % (start, len(out), i3))
-print('Coordinate = %d + %d + %d = %d' % (out[i1], out[i2], out[i3], out[i1] + out[i2] + out[i3]))
+start = order.index(out.index(0))
+print('Sum is %d' % sum(out[order[(start + offset) % len(order)]] for offset in [1000, 2000, 3000]))
 
-print('Not -15593062415802')
-print('Not 5871847521955')
-print('Not 5968426631162 is too high')
+
+#print('Not -15593062415802')
+#print('Not 5871847521955')
+#print('Not 5968426631162, too high')
